ecc003859727b5cc0040bec15e938a4460b4ea10
Gagan Diwan <gagan.diwan@icloud.com>
2021-10-11 16:48:42 +0300

    simple website scrapper

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 html_text = requests.get('https://duunitori.fi/tyopaikat/?haku=python&alue=').text
 soup = BeautifulSoup(html_text, 'lxml')
 jobs = soup.find_all('a',class_='job-box__hover gtm-search-result')
-first_job = jobs[0].text.replace(' ','')
+first_job = jobs[0].text.replace(' ','') #first element without spcaes
 print(first_job)
 
 
