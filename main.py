a834d59c33f30ec0a25d88151ea248fe02e8f47a
Gagan Diwan <gagan.diwan@icloud.com>
2021-10-11 16:42:27 +0300

    simple website scrapper

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import requests
 html_text = requests.get('https://duunitori.fi/tyopaikat/?haku=python&alue=').text
 soup = BeautifulSoup(html_text, 'lxml')
 jobs = soup.find_all('a',class_='job-box__hover gtm-search-result')
+first_job = jobs[0].text.replace(' ','')
+print(first_job)
 
 
 for job in jobs:
